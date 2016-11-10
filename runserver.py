acedb96d180edd54fae55ed366c9696a9146d21c
Fenichel <afenichel14@us164760.ad.aol.aoltw.net>
2016-11-10 09:19:17 -0500

    port error

--- runserver.py
+++ runserver.py
@@ -1,8 +1,13 @@
 from gunviolence import app
 import argparse
+import os
+import sys
 
 def main():
-	app.run(debug=True)
+	# app.run(debug=True)
+	# port = int(os.environ.get('PORT', 5000))
+	app.run(debug=True, host='0.0.0.0')#, port=port)
+
 
 
 def parse_args():
